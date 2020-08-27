2d5bf92e3d144ab8b69eb9f5b15d275f64fc0666
AkThao <akaash.thao@gmail.com>
2020-08-27 20:54:59 +0100

    Remove unnecessary build files

--- app_files/mac_target/PyInstaller/fbs_pyinstaller_hook.py
+++
@@ -1,3 +0,0 @@
-import importlib
-module = importlib.import_module('fbs_runtime._frozen')
-module.BUILD_SETTINGS = {'app_name': 'Sudoku Game', 'author': 'Akaash Thao', 'version': '1.0.0', 'environment': 'local'}
